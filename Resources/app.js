e82ee6be43305e3b1112b57a51804c6186ec03b2
Jerod Fritz <jerodfritz@gmail.com>
2012-11-26 13:39:29 -0500

    tweak

--- Resources/app.js
+++ Resources/app.js
@@ -21,7 +21,7 @@ button.addEventListener('click',function(){
     l.update({x1:10, y1:10, x2:10, y2:200,duration:2000})
     r.update({x1:200, y1:10, x2:200, y2:200,duration:2000})
     b.update({x1:10, y1:200, x2:200, y2:200,duration:2000})
-    t.update({x1:10, y1:10, x2:200, y2:100,duration:2000})
+    t.update({x1:10, y1:10, x2:200, y2:10,duration:2000})
   } else {
     l.update({x1:10, y1:10, x2:200, y2:10,duration:2000})
     r.update({x1:10, y1:200, x2:200, y2:200,duration:2000})
